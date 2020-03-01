66e1269507390281b73682456cf4d9d794d1534d
prismleong <981591636@qq.com>
2020-02-29 21:22:14 -0600

    Add files via upload

---
+++ HW2/PrimeFactors/Program.cs
@@ -0,0 +1,39 @@
+﻿using System; 
+
+
+namespace PrimeFactors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input;
+            string result="";
+            Console.WriteLine("请输入一个正整数：");
+            try { 
+                input=int.Parse(Console.ReadLine());
+                while (input % 2 == 0)
+                {
+                    result += "2 ";
+                    input /= 2;
+                }
+                int up = (int)Math.Sqrt(input * 1.0);
+                for (int i = 3; i < up; i+=2)
+                {
+                    while (input % i == 0)
+                    {
+                        result += i + " ";
+                        input /= i;
+                    }
+                }
+                if (input > 2) result += input + "";
+                Console.WriteLine(result);
+            }
+            catch
+            {
+                Console.WriteLine("输入有误，请输入一个正整数");
+            }
+            
+        }
+    }
+}
---
+++ HW2/埃氏筛法/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace 埃氏筛法
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int up = 100;
+            bool[] isprime;
+            Console.WriteLine("请输入素数范围：");
+            try { up = int.Parse(Console.ReadLine()); }
+            catch { Console.WriteLine("请输入一个正整数"); }
+
+            isprime = new bool[up+1];
+            for (int i = 0; i < isprime.Length; i++) isprime[i] = true;
+            isprime[0] = isprime[1] = false;
+
+            for(int i = 2; i*i<=up; i++)
+            {
+                if (isprime[i])
+                {
+                    for (int j = i * 2; j <= up; j += i) isprime[j] = false;
+                }
+            }
+            for (int i = 0; i < isprime.Length; i++)
+            {
+                if (isprime[i])
+                    Console.Write(i+" ");
+                if(i%10==0) Console.Write('\n');
+            }
+        }
+    }
+}
